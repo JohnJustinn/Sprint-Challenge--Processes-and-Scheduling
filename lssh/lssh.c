4b384d7d802056eb386fddfc44186f41e212142f
Johnathan Justinn <jjustinn85@gmail.com>
2018-04-13 13:08:11 -0400

    Print Error working, CD working to change directories

--- lssh/lssh.c
+++ lssh/lssh.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <errno.h>
@@ -11,7 +12,7 @@
 
 #define MAX_TOKENS 100
 #define COMMANDLINE_BUFSIZE 1024
-#define DEBUG 1  // Set to 1 to turn on some debugging output, or 0 to turn off
+#define DEBUG 1 // Set to 1 to turn on some debugging output, or 0 to turn off
 
 /**
  * Parse the command line.
@@ -36,12 +37,13 @@
 char **parse_commandline(char *str, char **args, int *args_count)
 {
     char *token;
-    
+
     *args_count = 0;
 
     token = strtok(str, " \t\n\r");
 
-    while (token != NULL && *args_count < MAX_TOKENS - 1) {
+    while (token != NULL && *args_count < MAX_TOKENS - 1)
+    {
         args[(*args_count)++] = token;
 
         token = strtok(NULL, " \t\n\r");
@@ -67,7 +69,8 @@ int main(void)
     int args_count;
 
     // Shell loops forever (until we tell it to exit)
-    while (1) {
+    while (1)
+    {
         // Print a prompt
         printf("%s", PROMPT);
         fflush(stdout); // Force the line above to print
@@ -76,62 +79,64 @@ int main(void)
         fgets(commandline, sizeof commandline, stdin);
 
         // Exit the shell on End-Of-File (CRTL-D)
-        if (feof(stdin)) {
+        if (feof(stdin))
+        {
             break;
         }
 
         // Parse input into individual arguments
         parse_commandline(commandline, args, &args_count);
 
-        if (args_count == 0) {
+        if (args_count == 0)
+        {
             // If the user entered no commands, do nothing
             continue;
         }
 
         // Exit the shell if args[0] is the built-in "exit" command
-        if (strcmp(args[0], "exit") == 0) {
+        if (strcmp(args[0], "exit") == 0)
+        {
             break;
         }
-
-        // Uses chDir() to change directory
-
+        // Change Directory using chDir()
         else if (strcmp(args[0], "cd") == 0)
         {
+
             char *homeDirectory = getenv("HOME");
             char *directory = (args_count > 1) ? args[1] : homeDirectory;
 
-            if (chdir(directory) == 0)
+            if (chdir(directory) == -1)
             {
-                char *getcwd_buffer;
-                char *currentDirectory = getcwd(getcwd_buffer, 128);
-                printf("Current directory changed to '%s'\n", currentDirectory);
-            } else
-            {
-                printf("Failed to change directoy\n");
-                continue;
+                perror("chdir");
             }
+
+            continue;
         }
 
-        #if DEBUG
+#if DEBUG
 
         // Some debugging output
 
         // Print out the parsed command line in args[]
-        for (int i = 0; args[i] != NULL; i++) {
+        for (int i = 0; args[i] != NULL; i++)
+        {
             printf("%d: '%s'\n", i, args[i]);
         }
 
-        #endif
-        
-        /* Add your code for implementing the shell's logic here */
-        if(fork() == 0)
+#endif
+
+        // Implement Here
+
+        if (fork() == 0)
         {
+            // Child Process
             execvp(args[0], args);
-        } else
+        }
+        else
         {
+            // Parent Process
             wait(NULL);
         }
-        
     }
 
     return 0;
